2987dc07c1d17a66ba30fb6921a1f84ce30fa87e
Baruch Murphy <baruchmurphy@gmail.com>
2021-01-04 15:35:27 -0800

    initial setup

---
+++ src/Components/header/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import Image from "../Images/"
+import "./header.css";
+
+const Header = () => {
+    return (
+        <div className="headerPic">
+            <h1></h1>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/Yelp.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Components/header/header";
+
+const Yelp = () => {
+    return(
+        <div>
+            <Header />
+        </div>
+    );
+};
+
+export default Yelp
